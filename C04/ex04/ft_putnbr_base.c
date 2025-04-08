e47d73900e93b461a303b0e3bfc50cf411ced534
Ka Shin Tan <ka-tan@c2r6s10.42singapore.sg>
2025-04-08 22:45:25 +0800

    base

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:24:42 by ka-tan            #+#    #+#             */
-/*   Updated: 2025/04/05 22:37:11 by ka-tan           ###   ########.fr       */
+/*   Updated: 2025/04/08 21:41:06 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,119 +14,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int	valid_base(char *base)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	j = 0;
-	while(base[i] != '\0')
+	while (base[i] != '\0')
 	{
-		if (base[i] == '+' || base[i] == '-' || base[i] <= 32 || base [i] == 127)
+		if (base[i] == '+' || base[i] == '-'
+			|| base[i] <= 32 || base [i] == 127)
 			return (0);
-		
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
 	}
-	
-
-
+	if (i < 2)
+		return (0);
+	return (1);
 }
 
-
-
-void ft_putnbr_base(int nbr, char *base)
+void	ft_putnbr_base(int nbr, char *base)
 {
-	if (base == "" ||)
+	long	nb;
+	int		baselen;
+
+	nb = nbr;
+	baselen = 0;
+	if (!valid_base(base))
 		return ;
-
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
-	{
-		ft_putchar('-');
-		ft_putchar('2');
-		nb = 147483648;
-	}
+	while (base[baselen] != '\0')
+		baselen++;
 	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = nb * -1;
 	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-	{
-		ft_putchar(nb + '0');
-	}
+	if (nb >= baselen)
+		ft_putnbr_base(nb / baselen, base);
+	ft_putchar(base[nb % baselen]);
 }
 
-// int	main(void)
+// int main(void)
 // {
-// 	ft_putnbr(-2146483648);
-// 	ft_putchar('\n');
-// 	ft_putnbr(2146483647);
-// 	ft_putchar('\n');
-// 	ft_putnbr(488);
-// 	ft_putchar('\n');
-// 	ft_putnbr(0);
-// 	ft_putchar('\n');
-// }
+// 	struct {
+// 		int number;
+// 		char *base;
+// 	} tests[] = {
+// 		{42, "0123456789"},       
+// 		{-42, "0123456789"},      
+// 		{255, "01"},
+// 		{18, "0123456789ABCDEF"}, 
+// 		{77, "poneyvif"},
+// 		{-2147483648, "0123456789ABCDEF"}, // INT_MIN in hex → -80000000
+// 		{2147483647, "0123456789ABCDEF"},  // INT_MAX in hex → 7FFFFFFF
+// 	};
 
+// 	int i = 0;
+// 	int count = sizeof(tests) / sizeof(tests[0]);
 
-int	ft_atoi(char *str)
-{
-	int	i;
-	int	result;
-	int	sign;
+// 	while (i < count)
+// 	{
+// 		printf("Input Number: %d\n", tests[i].number);
+// 		printf("Base: \"%s\"\n",(tests[i].base));
 
-	i = 0;
-	result = 0;
-	sign = 1;
-	while ((str[i] == 32) || (str[i] >= 9 && str [i] <= 13))
-	{
-		str++;
-	}
-	while (str[i] == '-' || str [i] == '+')
-	{
-		if (str [i] == '-')
-			sign = sign * -1;
-		str++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (sign * result);
-}
-
-// int	main(void)
-// {
-// 	char str1[] = "1235456";
-// 	char str2[] = "    123";
-// 	char str3[] = "+456";
-// 	char str4[] = "abc1235";
-// 	char str5[] = "+45";
-// 	char str6[] = "1235abc";
-// 	char str7[] = "+-+-+564";
-// 	char str8[] = "-+--1235456";
-
-// 	printf("1 ft_atoi: %d   atoi: %d\n", ft_atoi(str1),atoi(str1));
-// 	printf("2 ft_atoi: %d   atoi: %d\n", ft_atoi(str2),atoi(str2));
-// 	printf("3 ft_atoi: %d   atoi: %d\n", ft_atoi(str3),atoi(str3));
-// 	printf("4 ft_atoi: %d   atoi: %d\n", ft_atoi(str4),atoi(str4));
-// 	printf("5 ft_atoi: %d   atoi: %d\n", ft_atoi(str5),atoi(str5));
-// 	printf("6 ft_atoi: %d   atoi: %d\n", ft_atoi(str6),atoi(str6));
-// 	printf("7 ft_atoi: %d\n", ft_atoi(str7));
-// 	printf("8 ft_atoi: %d\n", ft_atoi(str8));
+// 		ft_putnbr_base(tests[i].number, tests[i].base);
+// 		printf("\n\n");
+// 		i++;
+// 	}
+// 	return (0);
 // }
--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:24:42 by ka-tan            #+#    #+#             */
-/*   Updated: 2025/04/05 21:06:38 by ka-tan           ###   ########.fr       */
+/*   Updated: 2025/04/08 22:43:35 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi(char *str)
+int	valid_base(char *base)
 {
 	int	i;
-	int	result;
+	int	j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-'
+			|| base[i] <= 32 || base [i] == 127)
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (i < 2)
+		return (0);
+	return (1);
+}
+
+int	get_index(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	check_str(char *str, int *i)
+{
 	int	sign;
+
+	sign = 1;
+	while (str[*i] == ' ' || (str[*i] >= 9 && str[*i] <= 13))
+		(*i)++;
+	while (str[*i] == '+' || str[*i] == '-')
+	{
+		if (str[*i] == '-')
+			sign *= -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int		i;
+	int		sign;
+	int		digit;
+	int		baselen;
+	long	result;
 
 	i = 0;
 	result = 0;
-	sign = 1;
-	while ((str[i] == 32) || (str[i] >= 9 && str [i] <= 13))
+	baselen = 0;
+	if (!valid_base(base))
+		return (-1);
+	while (base[baselen] != '\0')
+		baselen++;
+	sign = check_str(str, &i);
+	digit = get_index(str[i], base);
+	while (str[i] != '\0' && digit != -1)
 	{
-		str++;
-	}
-	while (str[i] == '-' || str [i] == '+')
-	{
-		if (str [i] == '-')
-			sign = sign * -1;
-		str++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
+		result = result * baselen + digit;
 		i++;
+		digit = get_index(str[i], base);
 	}
 	return (sign * result);
 }
 
 // int	main(void)
 // {
-// 	char str1[] = "1235456";
-// 	char str2[] = "    123";
-// 	char str3[] = "+456";
-// 	char str4[] = "abc1235";
-// 	char str5[] = "+45";
-// 	char str6[] = "1235abc";
-// 	char str7[] = "+-+-+564";
-// 	char str8[] = "-+--1235456";
-
-// 	printf("1 ft_atoi: %d   atoi: %d\n", ft_atoi(str1),atoi(str1));
-// 	printf("2 ft_atoi: %d   atoi: %d\n", ft_atoi(str2),atoi(str2));
-// 	printf("3 ft_atoi: %d   atoi: %d\n", ft_atoi(str3),atoi(str3));
-// 	printf("4 ft_atoi: %d   atoi: %d\n", ft_atoi(str4),atoi(str4));
-// 	printf("5 ft_atoi: %d   atoi: %d\n", ft_atoi(str5),atoi(str5));
-// 	printf("6 ft_atoi: %d   atoi: %d\n", ft_atoi(str6),atoi(str6));
-// 	printf("7 ft_atoi: %d\n", ft_atoi(str7));
-// 	printf("8 ft_atoi: %d\n", ft_atoi(str8));
+// 	printf("%d\n", ft_atoi_base("   ---+--+1234ab567", "0123456789")); 
+// 	printf("%d\n", ft_atoi_base("1010", "01"));                        
+// 	printf("%d\n", ft_atoi_base("FF", "0123456789ABCDEF"));            
+// 	printf("%d\n", ft_atoi_base("en", "ponydash"));                    
 // }
